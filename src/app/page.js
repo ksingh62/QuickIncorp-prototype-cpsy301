63437502032ea0a7b94322a97a6959a9e6ee61bb
aulakhsahib <sahibaulakh2003@gmail.com>
2024-04-18 14:19:47 -0600

    added advisor container

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import "./page.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Sidebar from "@/components/Sidebar/Sidebar";
@@ -8,8 +9,59 @@ export default function Home() {
       <Sidebar />
       <div>
         <Navbar />
-        <div className="advisor-container">
-          
+        <div className="advisor-parent-container">
+          <div className="advisor-container">
+            <h2 className="advisor-container-heading">Financial Advisors</h2>
+
+            <div className="advisor-card-container">
+              <div className="advisor-card">
+                <img src="/pic-2.jpg" alt="" />
+                <p>Keith Piper</p>
+              </div>
+
+              <div className="advisor-card">
+                <img src="/pic-3.jpg" alt="" />
+                <p>Andrew Scott</p>
+              </div>
+
+              <div className="advisor-card">
+                <img src="/pic-4.jpg" alt="" />
+                <p>Abigail Nolan</p>
+              </div>
+
+              <div className="advisor-card">
+                <img src="/pic-1.jpg" alt="" />
+                <p>Faith Morris</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="advisor-container">
+            <h2 className="advisor-container-heading">Legal Advisors</h2>
+
+            <div className="advisor-card-container">
+              <div className="advisor-card">
+                <img src="/pic-2.jpg" alt="" />
+                <p>Keith Piper</p>
+              </div>
+
+              <div className="advisor-card">
+                <img src="/pic-3.jpg" alt="" />
+                <p>Andrew Scott</p>
+              </div>
+
+              <div className="advisor-card">
+                <img src="/pic-4.jpg" alt="" />
+                <p>Abigail Nolan</p>
+              </div>
+
+              <div className="advisor-card">
+                <img src="/pic-1.jpg" alt="" />
+                <p>Faith Morris</p>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
